052694dc9825e44242fc1d05b76c3696956769c6
SANAYEE <dudtkd0219@gmail.com>
2022-08-02 23:55:07 +0900

    data model and main python

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+
+from PersonProfile import PersonProfile
 app = FastAPI()
+
+from fastapi.responses import FileResponse
 
 @app.get("/")
 def Name():
     return "hello"
+
+@app.get("/data")
+def Name():
+    return ['docs', 'compile', 'dodo']
+
+@app.get("/main")
+def Name():
+    return FileResponse('index.html')
+
+
+
+@app.post("/send")
+def Name(data : PersonProfile):
+    print(data)
+    return 'send complete'
